b44ca21e098c2dcb955fec084ee2950c8b10d8b3
Fritz Hund <edifice444@gmail.com>
2022-04-10 04:32:36 +0200

    Added RegEx example

--- Tools/Isaac JSON Tool/main.py
+++ Tools/Isaac JSON Tool/main.py
@@ -1,16 +1,31 @@
 import json
+import re
+
+def translateRewards(rewardsString):
+    rewardsLua = "rewards = "
+    cents = loot = treasure = souls = 0
+    
+    m = re.search("(?<=(REWARD|Reward):\s)(.*\n?)*", rewardsString)
+    if m != None:
+        print(m.group(0))
+    else:
+        m = re.search("(?<=(REWARDS|Rewards):\s)(.*\n?)*", rewardsString)
+        if m != None:
+            print(m.group(0))
+    
+    rewardsLua = rewardsLua + "{cents = " + str(cents) + ", loot = " + str(loot) + ", treasure = " + str(treasure) \
+        + ", souls = " + str(souls) + "}"
+    return rewardsLua
 
 if __name__ == "__main__":
     print("test")
     JSON_file = open("TS_Save_44.json", "r", encoding = "utf8")
     JSON_data = json.load(JSON_file)
-    counter = 0
     JSON_data = JSON_data["ObjectStates"]
     for val in JSON_data:
-        item = json.loads(json.dumps(val))
-        if item["Name"] == "Deck":
-            print(item["Nickname"])
-            counter += 1
-    print(counter)
+        #print(val["Name"])
+        if val["Name"] == "Deck" and val["Nickname"] == "Monsters cards":
+            for card in val["ContainedObjects"]:
+                print(translateRewards(card["Description"]))
     JSON_file.close()
     
