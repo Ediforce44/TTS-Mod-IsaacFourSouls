faf1b4efd68f7ae04dcc8dac7d86f6348381ba28
Fritz Hund <edifice444@gmail.com>
2023-11-18 10:51:26 +0100

    Added Counter script

---
+++ Tabletop Simulator Lua/Counter.4ba677.lua
@@ -0,0 +1,127 @@
+MIN_VALUE = -99
+MAX_VALUE = 999
+
+value = 0
+
+local function updateValue()
+    self.editButton({
+        index = 0,
+        label = tostring(value),
+        })
+end
+
+local function createAll()
+    local ttText = 
+[[Counter
+----------
+Left click - Increase
+Right click - Decrease]]
+
+    self.createButton({
+      label=tostring(value),
+      click_function="add_subtract",
+      tooltip=ttText,
+      function_owner=self,
+      position={0,0.05,-0.4},
+      height=600,
+      width=1000,
+      alignment = 3,
+      scale={x=1.5, y=1.5, z=1.5},
+      font_size=600,
+      font_color={1,1,1,95},
+      color={0,0,0,0}
+      })
+    self.createInput({
+      value = self.getName(),
+      input_function="dummy",
+      function_owner=self,
+      tooltip=ttText,
+      label = "Counter",
+      alignment = 3,
+      position = {0,0.05,1.4},
+      width = 1500,
+      height = 1000,
+      font_size = 200,
+      scale={x=1, y=1, z=1},
+      font_color= {1,1,1,95},
+      color = {0,0,0,0}
+      })
+end
+
+function add_subtract(_obj, _color, alt_click)
+    mod = alt_click and -1 or 1
+    new_value = math.min(math.max(value + mod, MIN_VALUE), MAX_VALUE)
+    if value ~= new_value then
+        value = new_value
+        updateValue()
+    end
+end
+
+function onLoad(saved_data)
+    if saved_data ~= "" then
+        local loaded_data = JSON.decode(saved_data)
+        if loaded_data then
+            if loaded_data.value then
+                value = loaded_data.value
+            end
+        end
+    end
+
+    createAll()
+end
+
+function onSave()
+    return JSON.encode({value = value})
+end
+
+function dummy()
+end
+
+-----------------------------------------------------------------------------------------------------------------------
+------------------------------------------------- External functions --------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+function modifyCounter(params)
+    if params.modifier == nil then
+        Global.call("printWarning", {text = "Wrong parameters in coin counter function 'modifyCounter()'."})
+    end
+    new_value = math.min(math.max(value + params.modifier, MIN_VALUE), MAX_VALUE)
+    if value ~= new_value then
+        value = new_value
+        updateValue()
+    end
+end
+
+function setCounter(params)
+    if params.value == nil then
+        Global.call("printWarning", {text = "Wrong parameters in coin counter function 'setCounter()'."})
+        return
+    end
+    new_value = math.min(math.max(params.value, MIN_VALUE), MAX_VALUE)
+    if value ~= new_value then
+        value = new_value
+        updateValue()
+    end
+end
+
+function changeName(params)
+    if params.name then
+        return
+    end
+
+    local newName = tostring(params.name)
+    self.setName(newName)
+    self.editInput({
+        index = 0,
+        value = newName
+    })
+end
+
+function removeAll()
+    self.removeInput(0)
+    self.removeButton(0)
+end
+
+function reloadAll()
+    removeAll()
+    createAll()
+end
