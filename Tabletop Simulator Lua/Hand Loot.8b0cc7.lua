36193e9c644e395be518e54dea44dc15f142dc01
Fritz Hund <edifice444@gmail.com>
2023-11-29 19:30:37 +0100

    Improved hand counter

--- Tabletop Simulator Lua/Hand Loot.8b0cc7.lua
+++ Tabletop Simulator Lua/Hand Loot.8b0cc7.lua
@@ -5,17 +5,6 @@ value = 0
 LOOT_DECK_ZONE_GUID = Global.getTable("ZONE_GUID_DECK").LOOT
 COUNTER_MODULE = nil
 
---- Click delay time in seconds
-options = {["clickDelay"] = Global.getVar("CLICK_DELAY")}
-
-clickCounter = 0
-
-doubleClickParameters = {
-    ["identifier"] = "ClickTimer" .. self.guid,
-    ["function_name"] = "resetClickCounter",
-    ["delay"] = options.clickDelay,
-}
-
 function onSave()
     return JSON.encode({ownerColor = owner_color, value = value})
 end
@@ -23,12 +12,10 @@ end
 function onLoad(saved_data)
     COUNTER_MODULE = getObjectFromGUID(Global.getVar("COUNTER_MODULE_GUID"))
 
-    if saved_data == "" then
-        setOwner(nil)
-    else
+    if saved_data then
         local loaded_data = JSON.decode(saved_data)
         if loaded_data.ownerColor then
-            setOwner(loaded_data.ownerColor)
+            owner_color = loaded_data.ownerColor
         end
         if loaded_data.value then
             value = loaded_data.value
@@ -94,31 +81,11 @@ end
 
 function findOwner(color)
     if not Global.call("isPlayerAuthorized", {playerColor = color, ownerColor = owner_color}) then return end         --EbyE44
-    --[[if clickCounter == 0 then
-        Timer.create(doubleClickParameters)
-    end
-    if isDoubleClick() then
-        setOwner(nil)
-    elseif not owner_color and (color == "Red" or color == "Blue" or color == "Green" or color == "Yellow") then      --EbyE44
-        setOwner(color)
-    end]]
-    --Delete this if you want to use the owner funtion
     drawLootCard()
-end
-
-function onPickedUp(color)
-    findOwner(color)
 end
 
 function onClick(object, color)
     findOwner(color)
-end
-
-function resetClickCounter()
-    if clickCounter == 1 then
-        drawLootCard()    --EbyE44
-    end
-    clickCounter = 0
 end
 
 function drawLootCard()    --EbyE44
@@ -127,28 +94,6 @@ function drawLootCard()    --EbyE44
         return
     end
     getObjectFromGUID(LOOT_DECK_ZONE_GUID).call("dealLootCard", {playerColor = owner_color})
-end
-
--- This function is disabled
-function isDoubleClick()
-    clickCounter = clickCounter + 1
-    return false --clickCounter > 1
-end
-
-function setOwner(color)
-    if color then
-        owner_color = color
-        local handInfo = Global.call("getHandInfo")[owner_color]
-        value = #Player[handInfo.owner].getHandObjects(handInfo.index)
-        --self.setName(value)
-    else
-        owner_color = nil
-        color = "Grey"
-        value = 0
-        --self.setName("Card Counter - Click to claim!")
-    end
-    local rgbColorTable = stringColorToRGB(color)
-    self.setColorTint(rgbColorTable)
 end
 
 function dummy()
