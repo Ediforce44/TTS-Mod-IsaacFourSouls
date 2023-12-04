b8dac4bc6752b011da7bee9b238c29ae148904fb
Fritz Hund <edifice444@gmail.com>
2023-12-04 20:06:35 +0100

    Player Counter Zones don't detect bags that enter and leave the zone in one go

--- Tabletop Simulator Lua/Player Counter Zone.bd8676.lua
+++ Tabletop Simulator Lua/Player Counter Zone.bd8676.lua
@@ -1,25 +1,40 @@
 owner_color = "Blue"
 
+function attachCounterBag(counterModule, counterBag)
+    local counterBagStillInZone = false
+    for _, obj in pairs(self.getObjects()) do
+        if obj == counterBag then
+            counterBagStillInZone = true
+            break
+        end
+    end
+    if not counterBagStillInZone then
+        return
+    end
+    
+    counterBag.setLock(true)
+
+    local counterType = counterBag.getVar("COUNTER_TYPE") or counterBag.getGUID()
+    local counterBagTable = counterModule.getTable("COUNTER_BAGS")[counterType]
+
+    if counterBagTable then
+        for _, bag in pairs(counterBagTable) do
+            if counterBag == bag then
+                return
+            end
+        end
+    end
+
+    counterBag.setVar("COUNTER_TYPE", counterType)
+    counterModule.call("counterBag_attach", {counterBag = counterBag, type = counterType})
+end
+
 function onObjectEnterZone(zone, object)
     if zone == self then
         if (object.type == "Bag") or (object.type == "Infinite") then
             local counterModule = getObjectFromGUID(Global.getVar("COUNTER_MODULE_GUID"))
             if counterModule then
-                Wait.condition(function() object.setLock(true) end, function() return object.resting end)
-                
-                local counterType = object.getVar("COUNTER_TYPE") or object.getGUID()
-                local counterBagTable = counterModule.getTable("COUNTER_BAGS")[counterType]
-
-                if counterBagTable then
-                    for _, counterBag in pairs(counterBagTable) do
-                        if object == counterBag then
-                            return
-                        end
-                    end
-                end
-
-                object.setVar("COUNTER_TYPE", counterType)
-                counterModule.call("counterBag_attach", {counterBag = object, type = counterType})
+                Wait.condition(function() attachCounterBag(counterModule, object) end, function() return object.resting end)
             end
         end
     end
