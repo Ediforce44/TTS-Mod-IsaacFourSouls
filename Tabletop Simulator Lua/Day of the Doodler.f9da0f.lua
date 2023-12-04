6b6c1a8c910be2f368dd4ff570ea9d546dde3981
Fritz Hund <edifice444@gmail.com>
2023-12-04 11:04:32 +0100

    Added 2 challenges

---
+++ Tabletop Simulator Lua/Day of the Doodler.f9da0f.lua
@@ -0,0 +1,75 @@
+local CHALLENGE_MODULE = nil
+
+local CONTENT_TABLE = {
+    BOSS = "63f727",
+    RULE = "c0f842"
+    --skip for better performance
+    --MANUAL = "cd02b4"
+}
+
+local DIF_TO_STATE = {
+    BOSS = {
+        DIF_NORMAL = 1,
+        DIF_HARD = 2,
+        DIF_ULTRA = 3,
+        DIF_COMP = 4
+    },
+    RULE = {
+        DIF_NORMAL = 1,
+        DIF_HARD = 2,
+        DIF_ULTRA = 3,
+        DIF_COMP = 4
+    }
+}
+
+HAS_CUSTOM_SETTINGS = false
+HAS_SEP_COMP_MODE = true
+
+function onLoad(saved_data)
+    CHALLENGE_MODULE = getObjectFromGUID(Global.getVar("CHALLENGE_MODULE"))
+end
+
+local function extractContent(difficulty)
+    local extractedContent = {}
+    for id, guid in pairs(CONTENT_TABLE) do
+        local object = self.takeObject({guid = guid})
+        if object then
+            if DIF_TO_STATE[id] then
+                local currentStateID = object.getStateId()
+                if (currentStateID > 0) and (currentStateID ~= DIF_TO_STATE[id][difficulty]) then
+                    object = object.setState(DIF_TO_STATE[id][difficulty])
+                end
+            end
+            extractedContent[id] = object
+        end
+    end
+    return extractedContent
+end
+
+function presetupChallenge(params)
+    local extractedContent = extractContent(params.difficulty)
+    CHALLENGE_MODULE.call("placeChallengeContent", extractedContent)
+    self.setPositionSmooth(Global.getTable("CHALLENGE_LEFTOVERS_POSITION"), false)
+end
+
+function setupChallengeZones(params)
+    --TODO
+    --[[
+    local monsterGUIDs = params.preDeckGUIDs.MONSTER
+    local noGoTags = params.filterTags
+
+    for _, guid in pairs(monsterGUIDs) do
+        local preDeck = getObjectFromGUID(guid)
+        if preDeck then
+            for _, infoTable in pairs(preDeck.getObjects()) do
+                if string.match(infoTable.gm_notes, "rag_man$") then
+                    self.putObject(preDeck.takeObject({guid = infoTable.guid}))
+                end
+            end
+        end
+    end]]
+end
+
+function setupChallenge(params)
+    --TODO
+end
